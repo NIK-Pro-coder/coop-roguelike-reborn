14ae34bed22f0b94945a4bf9251086a830263a78
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-06-06 07:27:01 +0200

    Small fix to attack_state.gd

--- StateMachine/States/attack_state.gd
+++ StateMachine/States/attack_state.gd
@@ -5,14 +5,14 @@ class_name AttackState
 
 var attack_cooldown: float = 1.5
 
-func update(_delta: float, enemy: Enemy) -> void:
+func update(delta: float, enemy: Enemy) -> void:
   var diff: Vector2 = (enemy.global_position - enemy.target.global_position)
   
   if diff.length_squared() > max_dist * max_dist:
     transition("follow")
     return
   
-  attack_cooldown -= _delta
+  attack_cooldown -= delta
   if attack_cooldown <= 0.0:
     print("hi")
     attack_cooldown = 1.5
