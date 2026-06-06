94e6079bc63c3f059a933da2bf997d61ed177a55
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-06-06 07:32:25 +0200

    Create Hitbox and Hurtbox classes

---
+++ Hitbox/hitbox.gd
@@ -0,0 +1,16 @@
+@tool
+
+extends Area2D
+class_name Hitbox
+
+@export var size: Vector2 = Vector2(20, 20)
+
+var shape: CollisionShape2D
+
+func _ready() -> void:
+  shape = CollisionShape2D.new()
+  shape.shape = RectangleShape2D.new()
+  add_child(shape)
+
+func _process(_delta: float) -> void:
+  (shape.shape as RectangleShape2D).size = size
