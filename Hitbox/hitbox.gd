2befec1f082aeb7e3f29788f8c58203c4de4ae5a
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-06-09 08:17:25 +0200

    Added hitting and iframes to Hitboxes

--- Hitbox/hitbox.gd
+++ Hitbox/hitbox.gd
@@ -4,22 +4,30 @@ extends Area2D
 class_name Hitbox
 
 enum Teams {
-  Player = 2,
-  Enemy = 4,
+  Player = 4,
+  Enemy = 2,
 }
 
 @export var size: Vector2 = Vector2(20, 20)
 @export var team: Teams = Teams.Player
+@export var iframe_group: String = ""
+@export var iframe_length: float = .5
 
 var shape: CollisionShape2D
 
 func _ready() -> void:
-  collision_mask = 0
+  if iframe_group == "":
+    iframe_group = str(get_instance_id())
+  
+  collision_layer = 0
   
   shape = CollisionShape2D.new()
   shape.shape = RectangleShape2D.new()
   add_child(shape)
 
 func _process(_delta: float) -> void:
-  collision_layer = team
+  collision_mask = team
   (shape.shape as RectangleShape2D).size = size
+  
+  for i: Hurtbox in get_overlapping_areas():
+    i.hit(self)
--- Hitbox/hurtbox.gd
+++ Hitbox/hurtbox.gd
@@ -4,22 +4,38 @@ extends Area2D
 class_name Hurtbox
 
 enum Teams {
-  Player = 4,
-  Enemy = 2,
+  Player = 2,
+  Enemy = 4,
 }
 
 @export var size: Vector2 = Vector2(20, 20)
 @export var team: Teams = Teams.Player
 
 var shape: CollisionShape2D
+var iframes: Dictionary[String, float] = {}
 
 func _ready() -> void:
-  collision_layer = 0
+  collision_mask = 0
   
   shape = CollisionShape2D.new()
   shape.shape = RectangleShape2D.new()
   add_child(shape)
 
-func _process(_delta: float) -> void:
-  collision_mask = team
+func _process(delta: float) -> void:
+  for i in iframes:
+    iframes[i] -= delta
+    if iframes[i] <= 0.0:
+      iframes.erase(i)
+  
+  collision_layer = team
   (shape.shape as RectangleShape2D).size = size
+
+signal got_hit(from: Hitbox)
+
+func hit(hitbox: Hitbox) -> void:
+  if hitbox.iframe_group in iframes:
+    return
+  
+  iframes[hitbox.iframe_group] = hitbox.iframe_length
+  got_hit.emit(hitbox)
+  print(hitbox)
