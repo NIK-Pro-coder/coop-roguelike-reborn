30e43fe80cfb98cc9405e6621e4248e547486e91
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-06-09 07:36:38 +0200

    Added teams to Hitbox & Hurtbox

--- Hitbox/hitbox.gd
+++ Hitbox/hitbox.gd
@@ -3,14 +3,23 @@
 extends Area2D
 class_name Hitbox
 
+enum Teams {
+  Player = 2,
+  Enemy = 4,
+}
+
 @export var size: Vector2 = Vector2(20, 20)
+@export var team: Teams = Teams.Player
 
 var shape: CollisionShape2D
 
 func _ready() -> void:
+  collision_mask = 0
+  
   shape = CollisionShape2D.new()
   shape.shape = RectangleShape2D.new()
   add_child(shape)
 
 func _process(_delta: float) -> void:
+  collision_layer = team
   (shape.shape as RectangleShape2D).size = size
--- Hitbox/hurtbox.gd
+++ Hitbox/hurtbox.gd
@@ -3,14 +3,23 @@
 extends Area2D
 class_name Hurtbox
 
+enum Teams {
+  Player = 4,
+  Enemy = 2,
+}
+
 @export var size: Vector2 = Vector2(20, 20)
+@export var team: Teams = Teams.Player
 
 var shape: CollisionShape2D
 
 func _ready() -> void:
+  collision_layer = 0
+  
   shape = CollisionShape2D.new()
   shape.shape = RectangleShape2D.new()
   add_child(shape)
 
 func _process(_delta: float) -> void:
+  collision_mask = team
   (shape.shape as RectangleShape2D).size = size
