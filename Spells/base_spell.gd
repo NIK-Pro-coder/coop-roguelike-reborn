7b4b963c01901524663c87fb104aec69f5fde174
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-06-10 20:20:56 +0200

    Create base_spell.gd

--- Spells/base_spell.gd
+++ Spells/base_spell.gd
@@ -1,2 +1,12 @@
 extends Resource
 class_name Spell
+
+@export var name: String
+@export_multiline() var desc: String
+
+@export_range(0.1, 5.0, 0.01, "or_greater") var cooldown: float = 1.5
+@export_range(0.0, 5.0, 0.01, "or_greater") var chargeup: float = 0.0
+
+@warning_ignore("unused_parameter")
+func cast(player: Player, target_dir: Vector2) -> void:
+  pass
