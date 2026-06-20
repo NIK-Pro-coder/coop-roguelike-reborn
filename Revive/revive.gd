95d325971b0761256f38236f413aff52f8381eb9
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2026-06-20 16:07:36 +0200

    Changed revive vfx

--- Revive/revive.gd
+++ Revive/revive.gd
@@ -1,7 +1,6 @@
 extends Node2D
 class_name Revive
 
-@onready var chain: Line2D = %Chain
 @onready var circle: Sprite2D = %Circle
 
 @export var target_player: Player
@@ -16,23 +15,21 @@ func _process(delta: float) -> void:
   if len(Qol.get_alive_players()) == 0:
     queue_free()
   
-  chain.points = [Vector2.ZERO, target_player.global_position - global_position]
-  
   var diff: Vector2 = (target_player.global_position - global_position)
   var dist: float = diff.length_squared()
   var a_dist: float = max_dist - (max_dist / 2.0 * revive_progress)
   
-  chain.default_color = Color.RED
-  if dist <= a_dist * a_dist:
+  var can_continue: bool = dist <= a_dist * a_dist
+  
+  if can_continue:
     revive_progress += delta / 10.0
-    chain.default_color = Color.WHITE
-  chain.default_color.a = .5
   
   circle.scale = Vector2.ONE * (4.0 - revive_progress * 2.0)
   
   revive_progress = clamp(revive_progress, 0.0, 1.0)
   
   (circle.material as ShaderMaterial).set_shader_parameter("progress", revive_progress)
+  (circle.material as ShaderMaterial).set_shader_parameter("can_continue", can_continue)
 
   if revive_progress >= 1.0:
     target_player.revive()
